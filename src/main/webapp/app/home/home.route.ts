a20bf4895d793fa6d6bd5e44d498a523795a8acd
volcanohong <volcanohong@gmail.com>
2018-03-02 20:04:03 -0800

    home route config  - home user/admin

--- src/main/webapp/app/home/home.route.ts
+++ src/main/webapp/app/home/home.route.ts
@@ -1,12 +1,19 @@
-import { Route } from '@angular/router';
+import {Routes} from '@angular/router';
+import {HomeUserModule} from './home-user/home-user.module';
+import {HomeAdminModule} from './home-admin/home-admin.module';
 
-import { HomeComponent } from './';
-
-export const HOME_ROUTE: Route = {
-    path: '',
-    component: HomeComponent,
-    data: {
-        authorities: [],
-        pageTitle: 'home.title'
+export const HOME_ROUTE: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        loadChildren: () => HomeUserModule
+    },
+    {
+        path: 'admin',
+        loadChildren: () => HomeAdminModule
     }
-};
+];
